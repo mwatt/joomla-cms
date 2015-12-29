1b2d5cc7f7b493d1b22e16d370666b91136ff6a7
George Wilson <georgejameswilson@googlemail.com>
2015-12-29 13:46:02 +0000

    Use correct variable

--- administrator/components/com_users/models/user.php
+++ administrator/components/com_users/models/user.php
@@ -684,7 +684,7 @@ class UsersModelUser extends JModelAdmin
 		// Prune out the current user if they are in the supplied user ID array
 		$user_ids = array_diff($user_ids, array(JFactory::getUser()->id));
 
-		if (empty($pks))
+		if (empty($user_ids))
 		{
 			$this->setError(JText::_('COM_USERS_USERS_NO_ITEM_SELECTED'));
 
